e20ab6a1c0886e68b802f088622f000e70fe3f46
Michał Wawrzeńczyk <wawrzenczyk.m@yahoo.com>
2023-08-27 22:07:57 +0200

    for in loop - display dummy user data

--- loops-in-action/loops-in-action.js
+++ loops-in-action/loops-in-action.js
@@ -39,3 +39,32 @@ function highlightLinks() {
 }
 
 highlightLinksBtnElement.addEventListener("click", highlightLinks);
+
+// Display user data - "for in" loop
+
+const dummyUserData = {
+  firstName: "Michał",
+  lastName: "Wawrzeńczyk",
+  age: 28,
+};
+
+const displayUserDataBtnElement = document.querySelector("#user-data button");
+
+function displayUserData() {
+  // get access to <ul>
+  const ulElement = document.getElementById("output-user-data");
+  // add list items to the <ul> (one <li> to each property inside the object with data)
+  // helper variable (const key/propertyName)
+  for (const key in dummyUserData) {
+    // create list item <li>
+    const newUserDataListItemElement = document.createElement("li");
+    // store the output text (dynamically access the value of each property of data object)
+    const outputText = key.toUpperCase() + ": " + dummyUserData[key];
+    // set the text content for each list item
+    newUserDataListItemElement.textContent = outputText;
+    // append each <li> to the <ul> element
+    ulElement.append(newUserDataListItemElement);
+  }
+}
+
+displayUserDataBtnElement.addEventListener("click", displayUserData);
